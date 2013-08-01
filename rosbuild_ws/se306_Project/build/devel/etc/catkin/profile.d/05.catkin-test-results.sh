00b1b69cf9689330dee07c0916d7c336bb6af0a8
John Lambert <jlam730@aucklanduni.ac.nz>
2013-08-01 14:58:57 +1200

    first commit

---
+++ rosbuild_ws/se306_Project/build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/john/GitRepo/306-Swarm-Robotics-Project/rosbuild_ws/se306_Project/build/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
---
+++ rosbuild_ws/se306_Project/build/devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/john/GitRepo/306-Swarm-Robotics-Project/rosbuild_ws/se306_Project/build/devel/setup.sh"
+emulate zsh # back to zsh mode
