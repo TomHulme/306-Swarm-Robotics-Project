b6e8099e4dd63fe6c7b439f9f57702216259ddd8
Nicole <nicole@ubuntu.(none)>
2013-08-28 03:51:26 -0700

    sheepdogPos sub/pub added. Sheep running away not quite working yet.

--- se306Project/src/SheepNode.cpp
+++ se306Project/src/SheepNode.cpp
@@ -77,23 +77,32 @@ void SheepNode::sheepdogDangerCallback(geometry_msgs::Pose2D sheepdogMsg) {
 	//	eg. if it is 10 units away (or whatever distance seems appropriate), then terror is low, but exists. 
 	//       if it is at 6 units away, terror goes up again, and sheep move away from the dog.
 	//		 if it is at 3 units away or less, sheep runs away from dog.
-	//if (xDistanceDiff<=20||yDistanceDiff<=20){
-	//	scared(sdx,sdy);
-	//}
-}
+	if (xDistanceDiff<=5||yDistanceDiff<=5){
+		runaway(sdx,sdy);
+	}									
+	ROS_INFO("Robot 0 -- Current x position is: %f", px);
+	ROS_INFO("Robot 0 -- Current y position is: %f", py);
+			
+	
+};
+
+void runaway(float sdx, float sdy) {
+	ROS_INFO("x Distance between Sheepdog and Sheep: %f",sdx);
+	ROS_INFO("y Distance between Sheepdog and Sheep: %f",sdy);
+};
 
 
 void SheepNode::sheepEat(){
 	//subscribe to current position grass service
 	//use service to eat grass? (say eating, grass sends back ok to keep eating, or stop eating.
 		//if stop eating, change state to walking, publish GO to sheep_x/move (sheepMovePub) 
-}
+};
 
 void SheepNode::sheepWalk() {
 	//check if there is edible grass here
 		//then publish STOP to sheep_x/move (sheepMovePub) 
 	//else send move message
-}
+};
 
 void SheepNode::spin() {
 	//do things depending on SheepState
@@ -152,14 +161,14 @@ void SheepNode::spin() {
 		//ROS_INFO("Count: %d", count);
 		rate.sleep();
 	}
-}
+};
 	
 SheepNode::SheepNode() {//(int number) {
 	sheepNum = 0;
 	currentState = WALKING;
 	age = BIRTH;
 	
-}
+};
 
 void SheepNode::rosSetup(int argc, char **argv) {
 	std::ostringstream convert;
@@ -180,7 +189,7 @@ void SheepNode::rosSetup(int argc, char **argv) {
 	
 	SheepNode::spin();
 	
-}
+};
 
 int main(int argc, char **argv) {
 
@@ -192,5 +201,5 @@ int main(int argc, char **argv) {
 	SheepNode sheep = SheepNode();//(number);
 	
 	sheep.rosSetup(argc, argv);
-}
+};
 
